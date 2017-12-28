f9bd265d43c9a398355a4ed08b7dbac6e9b47946
sushma madirala <sushma.maddirala@wavemaker.com>
2017-12-28 05:45:08 +0000

    t323 New files added - [AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,googleapis,heartRate,hrdb,en.json] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,data,fonts,lib,logos,material,momentLocale,ngLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,Readme.txt,README.md]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Tesgitlab9_28dec",
+  "homePage" : "Main",
+  "name" : "Tesgitlab9_28dec",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
